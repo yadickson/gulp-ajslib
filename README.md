0b570fb7f63d22fedf7ef762bb5e3e986abbc6a3
Yadickson Soto <yadickson.soto@basedos.cl>
2017-11-10 15:49:27 -0300

    Add first librery application support

--- README.md
+++ README.md
@@ -1,2 +1,33 @@
 # gulp-ajslib
 Gulp Angular JS utils to build Library
+
+[![dependencies][dependencies-image]][dependencies-url]
+[![dev-dependencies][dev-dependencies-image]][dev-dependencies-url]
+[![Known Vulnerabilities][vulnerabilities-image]][vulnerabilities-url]
+[![npm version][npm-image]][npm-url]
+
+## Installation
+
+```bash
+npm install -g gulp-ajslib
+```
+
+## Generator ajslib
+
+See [generator-ajslib](https://github.com/yadickson/generator-ajslib)
+
+## License
+
+GPL-3.0 © [Yadickson Soto](https://github.com/yadickson)
+
+[dependencies-image]: https://david-dm.org/yadickson/gulp-ajslib/status.svg
+[dependencies-url]: https://david-dm.org/yadickson/gulp-ajslib?view=list
+
+[dev-dependencies-image]: https://david-dm.org/yadickson/gulp-ajslib/dev-status.svg
+[dev-dependencies-url]: https://david-dm.org/yadickson/gulp-ajslib?type=dev&view=list
+
+[vulnerabilities-image]: https://snyk.io/package/npm/gulp-ajslib/badge.svg
+[vulnerabilities-url]: https://snyk.io/package/npm/gulp-ajslib
+
+[npm-image]: https://badge.fury.io/js/gulp-ajslib.svg
+[npm-url]: https://badge.fury.io/js/gulp-ajslib
---
+++ index.js
@@ -0,0 +1,119 @@
+'use strict'
+
+const gulp = require('gulp');
+const rename = require('gulp-rename');
+const gulpif = require('gulp-if');
+const concat = require('gulp-concat');
+const addsrc = require('gulp-add-src');
+const mainNpmFiles = require('gulp-main-npm-files');
+const order = require('gulp-order');
+const angularFilesort = require('gulp-angular-filesort');
+const babel = require('gulp-babel');
+const series = require('stream-series');
+const inject = require('gulp-inject');
+const cache = require('gulp-cache');
+const uglify = require('gulp-uglify');
+const ngdocs = require('gulp-ngdocs-components');
+
+const paths = {
+    srcScripts: ['src/scripts/**/*.js'],
+    srcTests: ['test/**/*.js']
+}
+
+function getOptions(options) {
+    return options || {};
+}
+
+function isMinimal(options) {
+    return getOptions(options).minimal === true;
+}
+
+function getDestination(options) {
+    return getOptions(options).dest || 'dist';
+}
+
+function getName(options) {
+    return getOptions(options).name || 'index';
+}
+
+function srcScripts() {
+    return gulp.src(paths.srcScripts)
+        .pipe(angularFilesort());
+}
+
+function buildScripts(options) {
+    var minimal = isMinimal(options);
+    var dest = getDestination(options);
+    var name = geName(options);
+    return srcScripts()
+        .pipe(gulpif(minimal, babel({
+            presets: ['env', 'minify']
+        })))
+        .pipe(gulpif(minimal, concat(name + '.js')))
+        .pipe(gulpif(minimal, uglify()))
+        .pipe(gulp.dest(dest));
+}
+
+function buildDocs(options) {
+    var dest = getDestination(options);
+    var options = {
+        scripts: []
+    };
+
+    return srcScripts()
+        .pipe(ngdocs.process(options))
+        .pipe(gulp.dest(dest))
+}
+
+function srcTestsScripts() {
+    return gulp.src(paths.srcTests)
+        .pipe(order([
+            '**/*_const_test.js',
+            '**/*_value_test.js',
+            '**/*_service_test.js',
+            '**/*_factory_test.js',
+            '**/*_provider_test.js',
+            '**/*_directive_test.js',
+            '**/*_filter_test.js',
+            '**/*_decorator_test.js',
+            '*'
+        ]));
+}
+
+function vendorTestScripts(options) {
+    var bootstrap = isBootstrap(options);
+    return gulp.src(mainNpmFiles()
+            .concat('!node_modules/**/index.js')
+            .concat('node_modules/angular/angular.js')
+            .concat('node_modules/angular-mocks/angular-mocks.js')
+        )
+        .pipe(order([
+            'jquery.js',
+            'angular.js',
+            'angular-ui-router.js',
+            'angular-mocks.js',
+            '*'
+        ]));
+}
+
+function updateKarmaFile(options) {
+    var configFile = getConfigFile(options);
+    var dest = getDestination(options);
+    return gulp.src(configFile)
+        .pipe(inject(series(vendorScripts(options), vendorTestScripts(options), srcScripts(), srcTestsScripts()), {
+            starttag: 'files: [',
+            endtag: '],',
+            relative: true,
+            transform: function(filepath, file, i, length) {
+                return '"' + filepath + '"' + (i + 1 < length ? ',' : '');
+            }
+        }))
+        .pipe(gulp.dest(dest));
+}
+
+module.exports = {
+    srcScripts: srcScripts,
+    srcTestsScripts: srcTestsScripts,
+    buildScripts: buildScripts,
+    updateKarmaFile: updateKarmaFile
+};
